ed9eac5a85e5e329ec4f90f6f28d9acf3b3814b6
Utkarsh Bodake <utkarsh.bodake@n26.com>
2018-12-06 11:13:32 +0100

    Scaffold for the serverless setup.

---
+++ transactions/TransactionsController.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const params = {
+  TableName: process.env.balanceTableName,
+};
+
+module.exports.list = (event, context, callback) => {
+  dynamoDb.scan(params, (error, result) => {
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the transactions.',
+      });
+      return;
+    }
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Items),
+    };
+    callback(null, response);
+  });
+};
