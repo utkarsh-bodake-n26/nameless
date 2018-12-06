eff3dda04979d1bce598add0af4801c1b83029d8
Abhijeet Pawar <abhijeet.pawar@n26.com>
2018-12-06 22:12:19 +0100

    Fix TxListener.

--- transactions/TransactionListener.js
+++ transactions/TransactionListener.js
@@ -2,25 +2,32 @@
 
 const ruleRepository = require("./RuleRepository");
 
+const transactionService = require("./TransactionsService");
+
 /*
-* SQS payload: {userId, tags, amount}
+* SQS payload: {userId: "id", tags: ["salary"], amount: 100, space: "main"}
 * */
 
 module.exports.process = (event, context, callback) => {
     const payload = event.Records[0].body;
     console.log("SQS payload: " + payload);
     const payloadObject = JSON.parse(payload);
-    console.log("SQS userId: " + payloadObject.userId);
+    let userId = payloadObject.userId;
 
-    ruleRepository.getRuleForUser(payloadObject.userId)
+    ruleRepository.getRuleForUser(userId)
         .then(result => {
-            if(payloadObject.txnTags.contains(result.txnTag)) {
-                const amountToTransfer = (payloadObject.amount * result.percentage) / 100
-                console.log(amountToTransfer)
+            if(payloadObject.tags.includes(result.Item.txnTag)) {
+                const amountToTransfer = (payloadObject.amount * result.Item.percentage) / 100;
+                console.log(amountToTransfer);
 
-
-
-
+                return transactionService.transferMoney(
+                    userId,
+                    result.Item.source_space,
+                    result.Item.destination_space,
+                    amountToTransfer
+                );
             }
         })
+        .then(() => console.log("SUCCESS!!!"))
+        .catch(() => console.log("FAILED!!!"))
 };
