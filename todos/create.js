be39a45cde96a27edc511f7b34326b7e7ef59435
rajat23 <rajat.chandak@n26.com>
2018-12-06 14:03:28 +0100

    basic intent manipulation with dynamoDB

--- todos/create.js
+++ todos/create.js
@@ -19,28 +19,28 @@ module.exports.create = (event, context, callback) => {
   }
 
   const params = {
-    TableName: process.env.DYNAMODB_TABLE,
-    Item: {
-      id: uuid.v1(),
-      text: data.text,
-      checked: false,
-      createdAt: timestamp,
-      updatedAt: timestamp,
-    },
-  };
+          TableName: process.env.DYNAMODB_TABLE,
+          Item: {
+              id: uuid.v1(),
+              text: data.text,
+              checked: false,
+              createdAt: timestamp,
+              updatedAt: timestamp,
+          },
+      };
 
-  // write the todo to the database
-  dynamoDb.put(params, (error) => {
-    // handle potential errors
-    if (error) {
-      console.error(error);
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t create the todo item.',
-      });
-      return;
-    }
+      // write the todo to the database
+      dynamoDb.put(params, (error) => {
+          // handle potential errors
+          if (error) {
+              console.error(error);
+              callback(null, {
+                  statusCode: error.statusCode || 501,
+                  headers: { 'Content-Type': 'text/plain' },
+                  body: 'Couldn\'t create the todo item.',
+              });
+              return;
+          }
 
     // create a response
     const response = {
--- transactions/TransactionsController.js
+++ transactions/TransactionsController.js
@@ -7,22 +7,75 @@ const params = {
   TableName: process.env.balanceTableName,
 };
 
-module.exports.list = (event, context, callback) => {
-  console.log('Received request for all transactions');
-  dynamoDb.scan(params, (error, result) => {
-    if (error) {
-      console.error(error);
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t fetch the transactions.',
-      });
-      return;
-    }
+module.exports.create = (event, context, callback) => {
+  console.log('Received request for all transactions',event);
 
-    const response = {
+  const requestBody = JSON.parse(event.body);
+  console.log("request body", requestBody);
+  console.log("query result", requestBody.queryResult);
+  const params = requestBody.queryResult.parameters;
+  const intent = requestBody.queryResult.intent.displayName;
+  console.log("intent", intent);
+    const dbParams = {
+        TableName: process.env.balanceTableName,
+        Item: {
+            userId: "abc",
+            spaceName: params.destination_space,
+            amount: params["unit-currency"].amount
+        },
+    };
+
+    dynamoDb.put(dbParams, (error) => {
+        // handle potential errors
+        if (error) {
+            console.error(error);
+            callback(null, {
+                statusCode: error.statusCode || 501,
+                headers: { 'Content-Type': 'text/plain' },
+                body: 'Couldn\'t create the todo item.',
+            });
+            return;
+        }
+
+     const response = {
       statusCode: 200,
-      body: JSON.stringify(result.Items),
+      body: JSON.stringify({
+          "fulfillmentText": "This is a text response",
+          "fulfillmentMessages": [
+              {
+                  "card": {
+                      "title": "card title",
+                      "subtitle": "card text",
+                      "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+                      "buttons": [
+                          {
+                              "text": "button text",
+                              "postback": "https://assistant.google.com/"
+                          }
+                      ]
+                  }
+              }
+          ],
+          "source": "example.com",
+          "payload": {
+              "google": {
+                  "expectUserResponse": true,
+                  "richResponse": {
+                      "items": [
+                          {
+                              "simpleResponse": {
+                                  "textToSpeech": "this is a simple response"
+                              }
+                          }
+                      ]
+                  }
+              }
+          },
+          "outputContexts": [
+          ],
+          "followupEventInput": {
+          }
+      }),
     };
     callback(null, response);
   });
