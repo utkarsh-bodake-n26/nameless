f63375a86ebbd7d8b04cb7356c7077d22a323cb3
Gopinath Langote <gopinathlangote11@gmail.com>
2018-12-07 10:06:28 +0100

    repo change

--- transactions/RuleRepository.js
+++ transactions/RuleRepository.js
@@ -5,12 +5,19 @@ const dynamoDb = require('../config/db');
 const table = {TableName: process.env.rulesTableName};
 
 const createRule = (userId, params) => {
-    const record = Object.assign({}, params, {userId: userId});
-    const dbParams = Object.assign({}, {Item: record}, table);
 
-    console.log(dbParams);
+    const saveParams = {
+        TableName: process.env.rulesTableName,
+        Item: {
+            userId: userId,
+            txnTag: params.txnTag,
+            percentage: params.percentage,
+            source_space: params.source_space,
+            destination_space: params.destination_space
+        }
+    };
     return new Promise((resolve, reject) => {
-        dynamoDb.getDoc().put(dbParams, (error, success) => {
+        dynamoDb.getDoc().put(saveParams, (error, success) => {
             if (error) reject(error);
             else resolve(success)
         });
